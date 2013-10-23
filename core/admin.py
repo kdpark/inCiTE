e9860b2aa16a59c32f9e86669608f85e6a0771e7
Kyle Park <wha754@gmail.com>
2013-10-23 17:24:52 +0900

    admin added

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from core.models import Participant
 
+class DisplayUser(admin.ModelAdmin):
+  list_display = ('name','extype' ,'article0', 'article1', 'article2', 'article3', 'article4', 'article5', 'article6', 'article7', 'article8', 'article9', 'article10', 'article11', 'article12', 'article13', 'article14')
 # Register your models here.
+
+admin.site.register(Participant, DisplayUser)
